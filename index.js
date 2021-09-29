5217808c9a8aa9475b2a4e6fc1ff55ec0dce96b9
Michael Airdrie <michaelairdrie@cmail.carleton.ca>
2021-09-29 06:27:06 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require ('express');
+const app = express();
+var exphbs  = require('express-handlebars');
+const path = require('path');
+
+//Express Middle Where???
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+const hey = "hey whats up hello!"
+
+//HandleBar Routes
+app.get('/', function (req, res) {
+    res.render('home', {
+    	stuff: "this is stuff...",
+    	hello: hey
+    });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.listen(PORT, () => console.log("Server listening on ", PORT));
+
