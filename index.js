36fa525866e7618edcdf442217b7425b641c5195
Michael Airdrie <michaelairdrie@cmail.carleton.ca>
2021-09-29 08:23:28 -0400

    adding comment

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //stock Market stuff
 const express = require ('express');
 const app = express();
-var exphbs  = require('express-handlebars');
+var exphbs = require('express-handlebars');
 const path = require('path');
 
 //Express Middle Where???
@@ -15,6 +15,12 @@ app.get('/', function (req, res) {
     res.render('home', {
     	stuff: "this is stuff...",
     	hello: hey
+    });
+});
+
+app.get('/about.html', function (req, res) {
+    res.render('about', {
+    	about: "In here I could type a bunch of stuff about my myself, it will then get converted into a paragraph on the about page"
     });
 });
 
