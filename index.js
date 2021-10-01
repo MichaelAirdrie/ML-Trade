1f948f461671d98f8eea8e8309e1460ca5aa0755
Michael Airdrie <michaelairdrie@cmail.carleton.ca>
2021-10-01 06:07:58 -0400

    created procfile for heroku

--- index.js
+++ index.js
@@ -3,20 +3,50 @@ const express = require ('express');
 const app = express();
 var exphbs = require('express-handlebars');
 const path = require('path');
+const request = require('request');
+const bodyParser = require('body-parser');
+var stockTicker = 'fb'
+var apiString = "https://cloud.iexapis.com/";
 
+
+//bodyParser middle where?
+app.use(bodyParser.urlencoded({extended: false}));
+
+function callApi(finishedApi, ticker){
+request(apiString + "stable/stock/" + ticker + "/quote?token=pk_5c761d09edfb4beabe74c628ef368295", {json: true}, (err, res, body) => {
+	if (err) { return console.log(err); }
+	if(res.statusCode === 200){ 
+		//console.log(body);
+		return finishedApi(body);
+	}
+});
+}
 //Express Middle Where???
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 const hey = "hey whats up hello!"
 
-//HandleBar Routes
+//HandleBar Routes2
+
 app.get('/', function (req, res) {
-    res.render('home', {
-    	stuff: "this is stuff...",
-    	hello: hey
-    });
+	callApi(function(doneApi, ticker) {
+		res.render('home', {
+    	stock: doneApi
+    	});
+	}, 'aapl');
 });
+
+app.post('/', function (req, res) {
+	
+	callApi(function(doneApi) {
+		//posted = req.body.stock_ticker;
+		res.render('home', {
+    	stock: doneApi
+    	});
+	}, req.body.stock_ticker);
+});
+//api key pk_5c761d09edfb4beabe74c628ef368295 
 
 app.get('/about.html', function (req, res) {
     res.render('about', {
