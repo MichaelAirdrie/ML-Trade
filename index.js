75329b3cb61fc017902f15c788821f3e87b947de
Michael Airdrie <michaelairdrie@cmail.carleton.ca>
2021-10-04 08:16:39 -0400

    Making fields requestable part 1

--- index.js
+++ index.js
@@ -7,13 +7,34 @@ const request = require('request');
 const bodyParser = require('body-parser');
 var stockTicker = 'fb'
 var apiString = "https://cloud.iexapis.com/";
-
-
+var apiDataType = "stable/stock/";
+const lookupFields = [
+	"latestPrice",
+	"change",
+	"changePercent",
+	"avgTotalVolume",
+	"week52High",
+	"week52Low"
+];
+function parseApiCall(filter, data){
+	var ret = {};
+	for (var i = 0; i < filter.length; i++){
+			var key = filter[i];
+			var dataf = data[filter[i]];
+			var obj = {key : dataf};
+			if (dataf){
+				ret += obj;	
+			}
+		}
+		return ret;
+};
+console.log(parseApiCall(lookupFields, {"latestPrice": "600", "change": "34", "hamburger": "no thanks"}));	
+const test = "Hello";
 //bodyParser middle where?
 app.use(bodyParser.urlencoded({extended: false}));
 
-function callApi(finishedApi, ticker){
-request(apiString + "stable/stock/" + ticker + "/quote?token=pk_5c761d09edfb4beabe74c628ef368295", {json: true}, (err, res, body) => {
+function callApi(finishedApi, ticker, apiDataType){
+request(apiString + apiDataType + ticker +"/quote?token=pk_5c761d09edfb4beabe74c628ef368295", {json: true}, (err, res, body) => {
 	if (err) { return console.log(err); }
 	if(res.statusCode === 200){ 
 		//console.log(body);
@@ -30,11 +51,12 @@ const hey = "hey whats up hello!"
 //HandleBar Routes2
 
 app.get('/', function (req, res) {
-	callApi(function(doneApi, ticker) {
+	callApi(function(doneApi, ticker, apiDataType) {
 		res.render('home', {
-    	stock: doneApi
+    	stock: doneApi,
+    	lookupFields: lookupFields
     	});
-	}, 'aapl');
+	}, 'aapl', apiDataType);
 });
 
 app.post('/', function (req, res) {
@@ -42,9 +64,10 @@ app.post('/', function (req, res) {
 	callApi(function(doneApi) {
 		//posted = req.body.stock_ticker;
 		res.render('home', {
-    	stock: doneApi
+    	stock: doneApi,
+    	lookupFields: lookupFields
     	});
-	}, req.body.stock_ticker);
+	}, req.body.stock_ticker, apiDataType);
 });
 //api key pk_5c761d09edfb4beabe74c628ef368295 
 
